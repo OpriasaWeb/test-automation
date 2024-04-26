81a3142fa5a4b7dfb225e6ab2e15715a0e5ae16d
Jeremy Opriasa <opriasajbusiness@gmail.com>
2024-04-27 02:19:46 +0800

    fix: modify sendKeys values during testing and debugging

--- test/bookcatalog/testBookCatalog.js
+++ test/bookcatalog/testBookCatalog.js
@@ -1,12 +1,13 @@
 const {By, until, Builder, Browser} = require('selenium-webdriver');
 const { Select } = require('selenium-webdriver/lib/select');
 require("chromedriver")
+// require("edgedriver")
 
 async function bookCatalog(){
   let driver
   let shouldQuit = false // flag to control when to quit the driver
   try{
-    driver = await new Builder().forBrowser("chrome").build()
+    driver = await new Builder().forBrowser("MicrosoftEdge").build()
 
     // Get and display my book catalog project (CRUD)
     await driver.get("http://localhost/bookcatalog/views/bookcatalog.php")
@@ -15,10 +16,10 @@ async function bookCatalog(){
 
     // NOTE: Test to fill-up inputs in the add modal
     try{
-      await driver.findElement({id: 'title'}).sendKeys("Let Me Know")
-      await driver.findElement({id: 'isbn'}).sendKeys("111213")
-      await driver.findElement({id: 'author'}).sendKeys("Lany only")
-      await driver.findElement({id: 'publisher'}).sendKeys("Album: While Driving")
+      await driver.findElement({id: 'title'}).sendKeys("Nyare guys")
+      await driver.findElement({id: 'isbn'}).sendKeys("123123")
+      await driver.findElement({id: 'author'}).sendKeys("Bruh")
+      await driver.findElement({id: 'publisher'}).sendKeys("Album: While Searching")
 
       // To select the category
       const selectElement = await driver.findElement({id: 'category'})
@@ -31,7 +32,7 @@ async function bookCatalog(){
         // Click the add button
         const addButton = await driver.findElement({ id: 'addbook' })
         await addButton.click()
-        console.log("Successfully add new book")
+        console.log("Test 1: Successfully add new book")
 
         // Modal after successfully add new book
         try{
@@ -40,7 +41,7 @@ async function bookCatalog(){
           // Wait until the button with the id: okMessageSuccess shows up
           await driver.wait(until.elementIsVisible(okSuccessModal));
           await okSuccessModal.click()
-          console.log("Refresh the table")
+          console.log("Test 2: Refresh the table")
         }
         catch(err){
           console.log("Error occured while handling success modal: ", err)
@@ -64,7 +65,7 @@ async function bookCatalog(){
 
       // Element found, do something with it
       await backButton.click()
-      console.log("Back add button clicked!")
+      console.log("Test 3: Back add button clicked!")
     } catch (error) {
       // Element not found, handle the error
       console.log("Back button not found:", error);
